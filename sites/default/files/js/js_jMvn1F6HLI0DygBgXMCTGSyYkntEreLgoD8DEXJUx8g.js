ceb6a6d730c2fd0f5c3d8d2f39d375fa0dbaeb4c
Aaron Froehlich <aaronfroehlich@gmail.com>
2021-11-06 15:27:27 -0400

    Updating gh-pages site

--- sites/default/files/js/js_jMvn1F6HLI0DygBgXMCTGSyYkntEreLgoD8DEXJUx8g.js
+++ sites/default/files/js/js_jMvn1F6HLI0DygBgXMCTGSyYkntEreLgoD8DEXJUx8g.js
@@ -845,29 +845,41 @@ document.documentElement.className += ' js';
 ;
 /**
  * @file
- * Colorbox Style JS.
  */
 
-(function ($) {
+(function ($, Drupal) {
 
-Drupal.behaviors.initColorboxDefaultStyle = {
-  attach: function (context, settings) {
-    $(context).bind('cbox_complete', function () {
-      // Only run if there is a title.
-      if ($('#cboxTitle:empty', context).length == false) {
-        $('#cboxLoadedContent img', context).bind('mouseover', function () {
-          $('#cboxTitle', context).slideDown();
+  "use strict";
+
+  Drupal.behaviors.initColorboxPlainStyle = {
+    attach: function (context, settings) {
+      $(context).bind('cbox_complete', function () {
+        // Make all the controls invisible.
+        $('#cboxCurrent, #cboxSlideshow, #cboxPrevious, #cboxNext', context).addClass('element-invisible');
+        // Replace "Close" with "×" and show.
+        $('#cboxClose', context).html('\u00d7').addClass('cbox-close-plain');
+        // Hide empty title.
+        if ($('#cboxTitle:empty', context).length == true) {
+          $('#cboxTitle', context).hide();
+        }
+        $('#cboxLoadedContent', context).bind('mouseover', function () {
+          $('#cboxClose', context).animate({opacity: 1}, {queue: false, duration: "fast"});
+          if ($('#cboxTitle:empty', context).length == false) {
+            $('#cboxTitle', context).slideDown();
+          }
         });
         $('#cboxOverlay', context).bind('mouseover', function () {
-          $('#cboxTitle', context).slideUp();
+          $('#cboxClose', context).animate({opacity: 0}, {queue: false, duration: "fast"});
+          if ($('#cboxTitle:empty', context).length == false) {
+            $('#cboxTitle', context).slideUp();
+          }
         });
-      }
-      else {
-        $('#cboxTitle', context).hide();
-      }
-    });
-  }
-};
+      });
+      $(context).bind('cbox_closed', function () {
+        $('#cboxClose', context).removeClass('cbox-close-plain');
+      });
+    }
+  };
 
-})(jQuery);
+})(jQuery, Drupal);
 ;
